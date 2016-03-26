8edf0c4ea64a66912a88843168c19609496c897b
jdetle <jdetle@gmail.com>
2016-03-25 22:38:43 -0700

    added functionality for pjax requests

--- source/github.js
+++ source/github.js
@@ -1,3 +1,14 @@
 // GitHub:
-$('img.rounded-2').hide();
-$('.vcard-fullname').hide();
+function hidePersonal() {
+   //console.log('running');
+   $('img.rounded-2').hide();
+   $('.vcard-fullname').hide();
+   $('img.gravatar').hide();
+   $('.pusher-is-only-committer').hide();
+}
+$(hidePersonal);
+
+$(document).on("pjax:complete", function() {
+   //console.log('complete');
+   $(hidePersonal);
+});
