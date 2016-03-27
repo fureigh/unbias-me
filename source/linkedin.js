427a4168662bc22ffbd60635553dfe59e22a836d
Fureigh <info@fureigh.com>
2016-03-26 17:32:00 -0700

    Hide name in incognito/logged-out mode too
    Closes https://github.com/fureigh/unbias-me/issues/10

--- source/linkedin.js
+++ source/linkedin.js
@@ -3,6 +3,9 @@ $('title').remove();
 $('#name-container').hide();
 $('.profile-picture').hide();
 
+// Logged out:
+$('h1#name').hide();
+
 // These should appear only if you're already connected, but just in case:
 $('.endorse-v2').hide();
 $('.write-endorsement').hide();
