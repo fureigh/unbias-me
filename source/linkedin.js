57677ead8b0b1d878e9011c47f8df9004d746690
Fureigh <info@fureigh.com>
2016-03-26 17:34:41 -0700

    LinkedIn: Hide 'Find a different [NAME]'
    Closes https://github.com/fureigh/unbias-me/issues/9

--- source/linkedin.js
+++ source/linkedin.js
@@ -6,6 +6,7 @@ $('.profile-picture').hide();
 // Logged out:
 $('h1#name').hide();
 $('.reg-upsell').hide();
+$('.name-search').hide();
 
 // These should appear only if you're already connected, but just in case:
 $('.endorse-v2').hide();
