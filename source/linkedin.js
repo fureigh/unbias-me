7d5f6c1586f0fc396f6c8ee2f7780c2dfb3837b2
Fureigh <info@fureigh.com>
2016-03-26 17:57:26 -0700

    LinkedIn search: hide profile pictures, obscure names
    Inspired by and closes https://github.com/fureigh/unbias-me/issues/13.

--- source/linkedin.js
+++ source/linkedin.js
@@ -11,3 +11,7 @@ $('.name-search').hide();
 // These should appear only if you're already connected, but just in case:
 $('.endorse-v2').hide();
 $('.write-endorsement').hide();
+
+// Search results:
+$('li.mod > a.result-image').hide();
+$('li.mod > .bd > h3 > a.main-headline').text('Person');
