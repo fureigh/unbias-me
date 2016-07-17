45121ef085aa18545f83fea729bf68707a17af1c
Chris Boette <cboette@gmail.com>
2016-07-17 12:10:11 -0500

    Move the hiding of page elements for GitHub into a CSS file.
    Move some logic into CSS file for LinkedIn. Refactor JS code.
    
    Move hiding of elements for Twitter from JS to CSS.
    
    Load CSS files for GitHub, LinkedIn, Twitter.

--- source/linkedin.js
+++ source/linkedin.js
@@ -1,56 +1,41 @@
-var result;
-
 // LinkedIn:
+'use strict';
+
 document.title = '';
-result = document.querySelector('#name-container');
-if (result) {
-  result.style.display = 'none';
-}
-result = document.querySelector('.profile-picture');
-if (result) {
-  result.style.display = 'none';
-}
 
-// Logged out:
-result = document.querySelector('h1#name');
-if (result) {
-  result.style.display = 'none';
-}
-result = document.querySelector('.reg-upsell');
-if (result) {
-  result.style.display = 'none';
-}
-result = document.querySelector('.name-search');
-if (result) {
-  result.style.display = 'none';
-}
+// Profile page:
 
-// These should appear only if you're already connected, but just in case:
-result = document.querySelector('.endorse-v2');
-if (result) {
-  result.style.display = 'none';
-}
-result = document.querySelector('.write-endorsement');
-if (result) {
-  result.style.display = 'none';
+var selectors = [
+  '#name-container',
+  '.profile-picture img',
+  '.insights',
+  '.right-fixed',
+  '#suggested-endorsements',
+  'h1#name'
+];
+
+selectors.forEach(function(sel) {
+  var result = document.querySelector(sel);
+  if (result) {
+    result.style.display = 'none';
+  }
+});
+
+var i, results;
+
+results = document.querySelectorAll('.reg-upsell');
+for (i = 0; i < results.length; i++) {
+  results[i].style.visibility = 'hidden';
 }
 
 // Search results:
-var i, results;
 
 results = document.querySelectorAll('li.mod > a.result-image');
 for (i = 0; i < results.length; i++) {
-  results[i].style.display = 'none';
+  results[i].style.visibility = 'hidden';
 }
 results = document.querySelectorAll('li.mod > .bd > h3 > a.main-headline');
 for (i = 0; i < results.length; i++) {
+  console.log('changed', results[i]);
   results[i].textContent = 'Person';
-}
-results = document.querySelectorAll('.profile-actions > .katy-button-group');
-for (i = 0; i < results.length; i++) {
-  results[i].style.display = 'none';
-}
-results = document.querySelectorAll('#siteflow-widget-next');
-for (i = 0; i < results.length; i++) {
-  results[i].style.display = 'none';
 }
--- source/twitter.js
+++ source/twitter.js
@@ -1,11 +1,3 @@
-var result;
+// Twitter:
 
 document.title = '';
-result = document.querySelector('img.ProfileAvatar-image');
-if (result) {
-  result.style.display = 'none';
-}
-result = document.querySelector('h1.ProfileHeaderCard-name');
-if (result) {
-  result.style.display = 'none';
-}
